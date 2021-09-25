fe793cfe22dd411a142657be38dca45ba5755ce1
Kartik Jetani <kartikjetani15@gmail.com>
2021-09-25 14:35:06 +0530

    localhost modified

--- client/src/components/inventory-list.js
+++ client/src/components/inventory-list.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router';
 
 export default class InventoryList extends Component {
     constructor(props) {
